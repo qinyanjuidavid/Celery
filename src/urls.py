0cefd664ccfe4778eb6585960704a768a1a6e415
day qinyanjui <49823575+qinyanjuidavid@users.noreply.github.com>
2021-07-19 15:31:29 +0300

    Created the celery script

--- src/urls.py
+++ src/urls.py
@@ -1,3 +1,6 @@
+from src.settings import BASE_DIR
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,3 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls', namespace="myapp"))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
